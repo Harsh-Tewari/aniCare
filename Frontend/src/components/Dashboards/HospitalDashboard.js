412760e9592d1136dcfb2a0e08211f739ae04ca0
Harsh Dutta Tewari <103374169+Harsh-Tewari@users.noreply.github.com>
2023-03-25 22:37:21 +0530

    Added All Dashboards except Gov

--- Frontend/src/components/Dashboards/HospitalDashboard.js
+++ Frontend/src/components/Dashboards/HospitalDashboard.js
@@ -1,65 +1,129 @@
-import React from 'react'
-import { useEffect } from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./puppyfarm.css";
+import { useEffect, useState } from "react";
 
 const HospitalDashboard = () => {
-    const fetchDataHospital=async()=>{
-      const email = localStorage.getItem("hospitalId")
-      const dat = { email }
-      const res = await fetch("/api/govLogin/fetchHospitalData", {
-        method: "POST",
-        headers: {
-          //always use this
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(dat),
-      });
-  
-      const check = await res.json();
-      console.log(check.data)
-    }
+  const [info, setinfo] = useState([]);
+  const fetchDataHospital = async () => {
+    const email = localStorage.getItem("hospitalId");
+    const dat = { email };
+    const res = await fetch("/api/govLogin/fetchHospitalData", {
+      method: "POST",
+      headers: {
+        //always use this
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(dat),
+    });
 
-    useEffect(() => {
-      fetchDataHospital();
-    }, [])
-    const accept=async()=>{
-      const email=localStorage.getItem("petParentEmail")
-      const data={email}
-      const res = await fetch("/api/petParent/accept", {
-        method: "POST",
-        headers: { 
-          //always use this
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      const check = await res.json();
-      console.log(check)
-    }
-    const decline=async()=>{
-      const email=localStorage.getItem("petParentEmail")
-      const data={email}
-      const res = await fetch("/api/petParent/decline", {
-        method: "POST",
-        headers: { 
-          //always use this
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      const check = await res.json();
-      console.log(check)
-    }
+    const check = await res.json();
+    console.log(check.data);
+  };
+
+  useEffect(() => {
+    fetchDataHospital();
+  }, []);
+  const accept = async () => {
+    const email = localStorage.getItem("petParentEmail");
+    const data = { email };
+    const res = await fetch("/api/petParent/accept", {
+      method: "POST",
+      headers: {
+        //always use this
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const check = await res.json();
+    console.log(check);
+  };
+  const decline = async () => {
+    const email = localStorage.getItem("petParentEmail");
+    const data = { email };
+    const res = await fetch("/api/petParent/decline", {
+      method: "POST",
+      headers: {
+        //always use this
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const check = await res.json();
+    console.log(check);
+  };
+
+  const fetchData = async () => {
+    const email = localStorage.getItem("vetname");
+    const dat = { email };
+    const res = await fetch("/api/govLogin/fetchHospitalData", {
+      method: "POST",
+      headers: {
+        //always use this
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(dat),
+    });
+
+    const check = await res.json();
+    console.log(check.data);
+    setinfo(check.data);
+    console.log(info);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      {/* code for render list from above api ..dutta do it */}
-      <button onClick={accept}>Accept</button>
-      <button onClick={decline}>Decline</button>
-
+    <div id="ppd">
+      <nav>
+        <img
+          src="https://res.cloudinary.com/dhg0kwh9c/image/upload/v1679721782/logo-removebg-preview1_jq1ybw.png"
+          alt=""
+          className="navimg"
+        />
+        <ul id="farmul">
+          <li>
+            <a href="/" target="_blank">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="centraldivppd">
+        <h1 id="pfhd">Hospital Dashboard</h1>
+        <table>
+          <tr>
+            <th>Breed</th>
+            <th>Contact No.</th>
+            <th>Parent Name</th>
+            <th>Approval</th>
+          </tr>
+          {info.map((item) => {
+            return (
+              <tr>
+                <td>{item.breed}</td>
+                <td>{item.phone}</td>
+                <td>{item.name}</td>
+                <td>
+                  <div className="poop">
+                    <button type="submit" onClick={accept} id="accept">
+                      accept
+                    </button>{" "}
+                    <button type="reset" onClick={decline} id="decline">
+                      decline
+                    </button>{" "}
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HospitalDashboard
+export default HospitalDashboard;
